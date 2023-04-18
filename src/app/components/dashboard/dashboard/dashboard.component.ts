2b667d1d658b41bb3b29e04f7f8691ee885f0c31
PC-HOUSE <gamer.ssjskrr@gmail.com>
2023-04-18 10:17:16 +0200

    add email

--- src/app/components/dashboard/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-dashboard',
@@ -8,6 +10,13 @@ import { ToastrService } from 'ngx-toastr';
 export class DashboardComponent implements OnInit {
   ngOnInit(): void {}
   constructor (
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private authFire: AngularFireAuth,
+    private router: Router,
   ) {}
+  logOut():void {
+    this.authFire.signOut()
+    this.toastr.info("Hasta Luego", "Sucess") 
+    this.router.navigate(['/login'])
+  }
 }
--- src/app/components/dashboard/send-mail/send-mail.component.ts
+++ src/app/components/dashboard/send-mail/send-mail.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-send-mail',
   templateUrl: './send-mail.component.html',
   styleUrls: ['./send-mail.component.css']
 })
-export class SendMailComponent {
+export class SendMailComponent implements OnInit {
+  ngOnInit(): void {}
 
+  constructor (
+    private toastr : ToastrService,
+    private authFire: AngularFireAuth,
+    private router: Router,
+  ){}
+
+  logOut():void {
+    this.authFire.signOut()
+    this.toastr.info("Hasta Luego", "Sucess") 
+    this.router.navigate(['/login'])
+  }
 }
